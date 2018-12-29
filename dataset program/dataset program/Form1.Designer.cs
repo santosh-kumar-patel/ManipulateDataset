d4389288413ea9d10c02c73be672eaf181df83ac
santosh <46041268+santosh-kumar-patel@users.noreply.github.com>
2018-12-29 15:48:39 +0530

    Add files via upload
    This is project of  C#. how to manipulate the data by Dataset .

---
+++ dataset program/dataset program/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace dataset_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGrid1 = new System.Windows.Forms.DataGrid();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(394, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(201, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(104, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(104, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Name";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(395, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 26);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(395, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 26);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(201, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(174, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(104, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Address";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(201, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(104, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Select ID";
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.DataMember = "";
+            this.dataGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGrid1.Location = new System.Drawing.Point(-2, 306);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(526, 206);
+            this.dataGrid1.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(395, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "First";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(395, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 26);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Last";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(201, 242);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(38, 31);
+            this.button6.TabIndex = 9;
+            this.button6.Text = " ◄ ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(238, 242);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(38, 31);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "►";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(383, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(102, 222);
+            this.panel1.TabIndex = 20;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Location = new System.Drawing.Point(32, 229);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(351, 56);
+            this.panel2.TabIndex = 17;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(32, 16);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(453, 49);
+            this.panel3.TabIndex = 19;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Location = new System.Drawing.Point(32, 65);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(67, 164);
+            this.panel4.TabIndex = 18;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel5.Location = new System.Drawing.Point(99, 65);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(284, 164);
+            this.panel5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(138, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 28);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Manipulate Dataset";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(520, 511);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGrid1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGrid dataGrid1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ dataset program/dataset program/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace dataset_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlDataAdapter adapter;
+        DataSet ds;
+        string con = "Data Source=NETDEVELOPER-PC;Initial Catalog=TBS;Integrated Security=True";
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            adapter = new SqlDataAdapter("select * from login", con);
+            ds = new DataSet();
+            adapter.Fill(ds,"employee");
+             new SqlCommandBuilder(adapter);
+             dataGrid1.DataSource = ds;
+            for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                comboBox1.Items.Add(ds.Tables[0].Rows[i][0]);
+            }
+          int j = ds.Tables[0].Rows.Count - 1;
+            if (j >= 0)
+            {
+                textBox1.Text = ds.Tables[0].Rows[0][0].ToString();
+                textBox2.Text = ds.Tables[0].Rows[0][1].ToString();
+                textBox3.Text = ds.Tables[0].Rows[0][2].ToString();
+            }
+            else
+            {
+                MessageBox.Show("No found data", "TCS", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //insert_button
+        {
+            try
+            {
+                DataRow row;
+                row = ds.Tables[0].NewRow();
+                row[0] = long.Parse(textBox1.Text);
+                row[1] = textBox2.Text;
+                row[2] = textBox3.Text;
+                ds.Tables[0].Rows.Add(row);
+                comboBox1.Items.Add(int.Parse(textBox1.Text));
+                adapter.Update(ds, "employee");
+                MessageBox.Show("inserted successfully");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox1.Focus();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)//delete_button
+        {
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+                {
+                    if (textBox1.Text.Equals(ds.Tables[0].Rows[i][0].ToString()))
+                    {
+                        comboBox1.Items.Remove(ds.Tables[0].Rows[i][0]);
+                        ds.Tables[0].Rows[i].Delete();
+
+                    }
+                }
+
+                adapter.Update(ds, "employee");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                comboBox1.Focus();
+            }
+            else
+            {
+                MessageBox.Show("No found data");
+            }
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)//added the id when we insert
+        {
+
+            for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                if (comboBox1.SelectedItem.ToString().Equals(ds.Tables[0].Rows[i][0].ToString()))
+                {
+                    textBox1.Text = ds.Tables[0].Rows[i][0].ToString();
+                    textBox2.Text = ds.Tables[0].Rows[i][1].ToString();
+                    textBox3.Text = ds.Tables[0].Rows[i][2].ToString();
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//update_button
+        {
+
+            for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                if (comboBox1.SelectedItem.ToString().Equals(ds.Tables[0].Rows[i][0].ToString()))
+                {
+                    ds.Tables[0].Rows[i][0] = int.Parse(textBox1.Text);
+                    ds.Tables[0].Rows[i][1] = textBox2.Text;
+                    ds.Tables[0].Rows[i][2] = textBox3.Text;
+                    comboBox1.Items[i] = ds.Tables[0].Rows[i][0];
+                }
+            }
+             adapter.Update(ds, "employee");
+             MessageBox.Show("update successfully");
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)//first_button
+        {
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                textBox1.Text = ds.Tables[0].Rows[0][0].ToString();
+                textBox2.Text = ds.Tables[0].Rows[0][1].ToString();
+                textBox3.Text = ds.Tables[0].Rows[0][2].ToString();
+            }
+            else
+            {
+                MessageBox.Show("No found data", "TCS", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)//last_button
+        {
+            int i = ds.Tables[0].Rows.Count - 1;
+            if (i >= 0)
+            {
+                textBox1.Text = ds.Tables[0].Rows[i][0].ToString();
+                textBox2.Text = ds.Tables[0].Rows[i][1].ToString();
+                textBox3.Text = ds.Tables[0].Rows[i][2].ToString();
+            }
+            else 
+            {
+                MessageBox.Show("No found data","TCS",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)//previous_button
+        {
+            int c = ds.Tables[0].Rows.Count -1;
+            for (int i = c; i >=1; i--)
+            {
+                if (ds.Tables[0].Rows[i][0].ToString().Equals(textBox1.Text))
+                {
+                    i--;
+                    textBox1.Text = ds.Tables[0].Rows[i][0].ToString();
+                    textBox2.Text = ds.Tables[0].Rows[i][1].ToString();
+                    textBox3.Text = ds.Tables[0].Rows[i][2].ToString();
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)//next_button
+        {
+            int c = ds.Tables[0].Rows.Count - 1;
+            for (int i = 0; i<c; i++)
+            {
+                if (ds.Tables[0].Rows[i][0].ToString().Equals(textBox1.Text))
+                {
+                    i++;
+                    textBox1.Text = ds.Tables[0].Rows[i][0].ToString();
+                    textBox2.Text = ds.Tables[0].Rows[i][1].ToString();
+                    textBox3.Text = ds.Tables[0].Rows[i][2].ToString();
+                }
+            }
+        }
+    }
+}
